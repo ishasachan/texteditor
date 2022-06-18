c4a03c7927d71823863d6ad8ef33e9c985e29570
ISHA SACHAN <79162510+ishasachan@users.noreply.github.com>
2022-06-18 17:07:39 +0530

    Initial commit

---
+++ src/components/TextForm.js
@@ -0,0 +1,57 @@
+import React, { useState } from "react";
+
+export default function TextForm(props) {
+  const handleUpClick = () => {
+    let newText = text.toUpperCase();
+    setText(newText);
+  };
+  const handleloClick = () => {
+    let newText = text.toLowerCase();
+    setText(newText);
+  };
+  const handleOnChange = (event) => {
+    setText(event.target.value);
+  };
+  const [text, setText] = useState("Enter text here");
+  return (
+    <>
+      <div
+        className="container"
+        style={{ color: props.mode === "dark" ? "white" : "#042743" }}
+      >
+        <h1>{props.heading}</h1>
+        <div className="mb-3">
+          <textarea
+            className="form-control"
+            style={{
+              backgroundColor: props.mode === "dark" ? "grey" : "white",
+              color: props.mode === "dark" ? "white" : "#042743"
+            }}
+            value={text}
+            onChange={handleOnChange}
+            id="myBox"
+            rows="8"
+          ></textarea>
+        </div>
+        <button className="btn btn-primary mx-2" onClick={handleUpClick}>
+          Convert to Uppercase
+        </button>
+        <button className="btn btn-primary mx-2" onClick={handleloClick}>
+          Convert to Lowercase
+        </button>{" "}
+      </div>
+
+      <div
+        className="container my-4"
+        style={{ color: props.mode === "dark" ? "white" : "#042743" }}
+      >
+        <h2>Your Text summary</h2>
+        <p>
+          {text.split(" ").length} words and {text.length} characters
+        </p>
+        <h2>Preview</h2>
+        <p>{text.length > 0 ? text : "Enter Something"}</p>
+      </div>
+    </>
+  );
+}
